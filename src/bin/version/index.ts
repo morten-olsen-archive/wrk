ac4472136812574a460f3589ce60b852ffa52c22
Morten Olsen <fbtijfdq@void.black>
2023-01-19 10:14:13 +0100

    fix: improved output

--- src/bin/version/index.ts
+++ src/bin/version/index.ts
@@ -1,11 +1,24 @@
-import { Command, program } from 'commander';
-import pkg from '../../../package.json';
+import { Command } from 'commander';
+import { info } from '../../info';
+import { formatOutput } from '../../utils/output';
+import { blue } from 'chalk';
 
 const setupVersion = (version: Command) => {
   version.action(async () => {
-    const { location } = program.opts();
-    const { version, name } = pkg;
-    console.log(location);
+    formatOutput(info, {
+      json: (input) => ({
+        name: input.name,
+        version: input.version,
+        paths: {
+          data: input.locations.data,
+        },
+      }),
+      rich: (input) => {
+        console.log(`${blue('name')}: ${input.name}`);
+        console.log(`${blue('version')}: ${input.version}`);
+        console.log(`${blue('data-location')}: ${input.locations.data}`);
+      } 
+    })
   });
 };
 
